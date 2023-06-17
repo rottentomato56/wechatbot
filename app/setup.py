2a33215c28283c2258b8155bb4a11460674ad52b
jason <jasonzhang56@gmail.com>
2023-06-17 13:27:15 -0700

    remove extraneous files

--- app/setup.py
+++
@@ -1,9 +0,0 @@
-import settings
-from celery import Celery
-from flask_caching import Cache
-
-cache = Cache(config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': settings.REDIS_URL})
-
-celery = Celery(broker=settings.CELERY_BROKER_URL)
-celery.config_from_object('settings')
-
